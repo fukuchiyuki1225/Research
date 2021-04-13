ceb4f4e7da7adde524187bed7883643dae1ff0a8
yuki-f <rkrn.zerozerozerooox@gmail.com>
2021-04-13 17:34:55 +0900

    特徴点マッチング＆物体検出＆ホモグラフィ変換行列取得表示まで完了

--- ImageRecognition/rotationalMovement.py
+++ ImageRecognition/rotationalMovement.py
@@ -30,3 +30,32 @@ for m,n in matches:
 
 # 物体検出に必要な対応点の数の下限
 MIN_MATCH_COUNT = 10
+
+if len(good) > MIN_MATCH_COUNT:
+    src_pts = np.float32([kp_q[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+    dst_pts = np.float32([kp_t[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+
+    # ホモグラフィ行列の推定(M:ホモグラフィ行列，mask:RANSACで使用されたか[0or1])
+    M, mask = cv2.findHomography(src_pts,dst_pts,cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+    print("行列")
+    print(M)
+
+    h,w = query.shape[:2] # query画像のサイズを取得
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2) # query画像の四隅の座標を行列に変換
+    dst = cv2.perspectiveTransform(pts,M) # Mをquery画像上の座標に変換
+
+    #train画像内で検出された部分を線で囲む
+    train = cv2.polylines(train,[np.int32(dst)],True,255,3,cv2.LINE_AA)
+
+    print(dst)
+    
+else:
+    print("Not enough matches are found")
+    matchesMask = None
+
+# 特徴点マッチを描画
+draw_params = dict(matchColor=(0,255,0),singlePointColor=None,matchesMask=matchesMask,flags=2)
+result = cv2.drawMatches(query,kp_q,train,kp_t,good,None,**draw_params)
+cv2.imshow("result",result)
+cv2.waitKey(0)
