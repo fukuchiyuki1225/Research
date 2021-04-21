7885428d205da9c571db77a001f26e4c2923fbb4
yuki-f <rkrn.zerozerozerooox@gmail.com>
2021-04-21 15:00:53 +0900

    引数でファイル名を渡すと不具合が起きたため一旦元に戻す

--- Filtering/getJsonFile.py
+++ Filtering/getJsonFile.py
@@ -3,14 +3,14 @@ import pandas as pd
 import urllib.request
 import time
 
-p_ids = pd.read_csv("./" + sys.argv[0])["p_id"].tolist()
+p_ids = pd.read_csv("./metadata.csv")["p_ID"].tolist()
 
 i = 0
 for p_id in p_ids:
     print(str(i))
     try:
         url = "https://projects.scratch.mit.edu/" + str(p_id)
-        savePass = "./" + sys.argv[1] + "/" + str(p_id) + ".json"
+        savePass = "./projectJSON/" + str(p_id) + ".json"
         urllib.request.urlretrieve(url, savePass)
     except:
         pass
