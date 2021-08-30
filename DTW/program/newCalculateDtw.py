c216f34d8407ef897a93567a3497244ab08f4736
fukuchiyuki1225 <rkrn.zerozerozerooox@gmail.com>
2021-08-30 16:13:38 +0900

    DTW距離を算出する関数移植＆各１動作の組み合わせごとにDTW距離を算出する関数を宣言（途中まで）

--- DTW/program/newCalculateDtw.py
+++ DTW/program/newCalculateDtw.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 # 各作品の時系列データを１つの動作ごとに分割
 def splitTsData(tsDataPath, savePath):
@@ -7,13 +8,10 @@ def splitTsData(tsDataPath, savePath):
         # 動作番号
         mvNum = 0
         for fileName in fileNames:
-            if fileName.startswith("."):
-                continue
-
             prjId = fileName.rsplit(".")[0]
 
             # 分割前のtsData
-            tsData = pd.read_csv(tsDataPath + "/" + fileName, usecols=[1, 2, 4, 5])
+            tsData = pd.read_csv(tsDataPath + "/" + fileName, usecols=["time", "sprite", "x", "y"])
 
             # 最後にスライスした行番号を保持
             sliceNum = 0
@@ -70,4 +68,88 @@ def checkData(outputData):
             return False
     return True
 
-splitTsData("/Users/yuki-f/Documents/SocSEL/Research/ImageRecognition/dataset/tsData5", "/Users/yuki-f/Documents/SocSEL/Research/ImageRecognition/dataset/splitted5")
+# 各1動作の組み合わせごとにDTW距離を算出
+def calculateDtw(splittedDataPath):
+    for pathName, dirName, fileNames in os.walk(splittedDataPath):
+        # 1動作のcsvを読み込み，配列に保持（配列の番号=動作番号）
+        splittedData = []
+        for i in range(len(fileNames)):
+            splittedData.insert(i, pd.read_csv(splittedDataPath + "/" + str(i) + ".csv", usecols=["x", "y"]).values)
+            
+# DTW距離を算出
+def dtw(x, y):
+    # xのデータ数，yのデータ数をそれぞれTx,Tyに代入
+    Tx = len(x)
+    Ty = len(y)
+    
+    # C:各マスの累積コスト，　B：最小コストの行/列番号
+    C = np.zeros((Tx, Ty))
+    B = np.zeros((Tx, Ty, 2), int)
+    
+    # 一番初めのマスのコストを，xとyのそれぞれ一番初めの値にする
+    C[0, 0] = dist(x[0], y[0])
+    
+    # 動的計画法を用いる
+    # 左下のマスからスタートし，各マスに到達するため最小の累積コストを1マスずつ求める
+    
+    # 境界条件：両端が左下と右上にあること
+    # 単調性：左下から始まり，右，上，右上のいずれかにしか進まないこと
+    # 連続性：繋がっていること
+    
+    # 一番下の行は，真っ直ぐ右にコストが累積される
+    for i in range(Tx):
+        C[i, 0] = C[i - 1, 0] + dist(x[i], y[0])
+        B[i, 0] = [i - 1, 0]
+        
+    # 同様に一番左の列は，真っ直ぐ上にコストが累積される
+    for j in range(1, Ty):
+        C[0, j] = C[0, j - 1] + dist(x[0], y[j])
+        B[0, j] = [0, j - 1]
+        
+    # その他のマスの累積コストを求める
+    for i in range(1, Tx):
+        for j in range(1, Ty):
+            pi, pj, m = get_min(C[i - 1, j],
+                                C[i, j - 1],
+                                C[i - 1, j - 1],
+                                i, j)
+            # get_minで返ってきた最小コストを累積コストに足す
+            C[i, j] = dist(x[i], y[j]) + m
+            # get_minで返ってきた最小コストの行/列番号を保持
+            B[i, j] = [pi, pj]
+    # 最終的な右上（最終の到達点）のコスト
+    cost = C[-1, -1]
+    
+    path = [[Tx - 1, Ty - 1]]
+    
+    # 逆順にたどることでパスを求める
+    i = Tx - 1
+    j = Ty - 1
+    
+    while((B[i, j][0] != 0) or (B[i, j][1] != 0)):
+        path.append(B[i, j])
+        i, j = B[i, j].astype(int)
+    path.append([0, 0])
+    return np.array(path), cost, C
+
+# 距離算出
+def dist(a, b):
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
+
+# 最小値算出
+def get_min(m0, m1, m2, i, j):
+    if m0 < m1:
+        if m0 < m2:
+            return i - 1, j, m0
+        else:
+            return i - 1, j - 1, m2
+    else:
+        if m1 < m2:
+            return i, j - 1, m1
+        else:
+            return i - 1, j - 1, m2
+
+        
+# splitTsData("/Users/yuki-f/Documents/SocSEL/Research/ImageRecognition/dataset/tsData5", "/Users/yuki-f/Documents/SocSEL/Research/ImageRecognition/dataset/splitted5")
+
+calculateDtw("/Users/yuki-f/Documents/SocSEL/Research/ImageRecognition/dataset/splitted5")
